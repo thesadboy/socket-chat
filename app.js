3a775012f94c230661e58ec85b607c4c5de1ad16
thesadboy <thesadboy@qq.com>
2013-08-07 23:45:18 +0800

    更新端口

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express'),
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 //app.set('env','production');
